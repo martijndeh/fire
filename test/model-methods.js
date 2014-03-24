9d33a36d859517a829c47c5deb4bb1f87b7785ca
martijndeh <martijn@ff00ff.nl>
2014-03-24 11:33:21 +0100

    fixed Reference when model is already loaded

--- test/model-methods.js
+++ test/model-methods.js
@@ -1,3 +1,26 @@
 var fire = require('..');
+var Models = require('./../lib/models');
 
-var should = require('chai').should()
+describe('models', function() {
+    var models;
+    before(function() {
+        models = new Models();
+        models.setup(null);
+    });
+
+    it('can reference model', function(done) {
+        function ModelOne() {
+            this.name = [this.Text];
+        }
+
+        models.addModel(ModelOne);
+
+        function ModelTwo(models) {
+            this.ref = [this.Reference(models.ModelOne)]
+        }
+
+        models.addModel(ModelTwo);
+
+        done();
+    });
+})
