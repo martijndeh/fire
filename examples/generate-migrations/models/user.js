12917422838bb923fe5c6a66d64a4ad8f3da40fe
martijndeh <martijn@ff00ff.nl>
2014-06-03 00:03:42 +0200

    fix generating migrations

--- examples/generate-migrations/models/user.js
+++ examples/generate-migrations/models/user.js
@@ -1,8 +1,11 @@
 exports = module.exports = User;
 
-function User(models) {
+var fire = require('./../../../');
+
+function User() {
 	this.name = [this.String, this.Required];
 	this.password = [this.String];
 	this.email = [this.String];
 	this.value = [this.Integer, this.Default(123)];
 }
+fire.model(User);
