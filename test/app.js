223ef8d0c799e2bc0a39fc31e53a2534699a6533
martijndeh <martijn@ff00ff.nl>
2014-08-13 15:45:54 +0200

    fixes app#configure

--- test/app.js
+++ test/app.js
@@ -1,14 +1,28 @@
-var fire = require('..');
+/* global describe, beforeEach, before, afterEach, it */
+'use strict';
+
+var helper = require('./support/helper');
+var assert = require('assert');
 
 describe('app', function() {
-	it('should run & stop', function(done) {
-		var app = fire.app('test', {});
-		return app.run()
-			.then(function() {
-				return app.stop();
-			})
-			.then(function() {
-				return done();
-			});
-	})
-})
+	beforeEach(helper.beforeEach());
+	afterEach(helper.afterEach());
+
+	describe('configure', function() {
+		var called = false;
+
+		before(function() {
+			helper.setup = function() {
+				helper.app.configure(function() {
+					assert.equal(this, helper.app);
+					called = true;
+				});
+			};
+		});
+
+		it('can configure app', function(done) {
+			assert.equal(called, true);
+			done();
+		});
+	});
+});
