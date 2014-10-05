e20832ec6b61bbcdc9545631d8831eec0bb7a3ca
martijndeh <martijn@ff00ff.nl>
2014-10-05 11:24:24 +0200

    fixes value parameter orders

--- test/models-find-with-association-property.js
+++ test/models-find-with-association-property.js
@@ -3,13 +3,22 @@
 
 var helper = require('./support/helper');
 var assert = require('assert');
+var Q = require('q');
 
 describe('models find with association property', function() {
 	beforeEach(helper.beforeEach());
 	afterEach(helper.afterEach());
 
+	var userID = null;
+
 	before(function() {
 		helper.setup = function(app) {
+			function User() {
+				this.name = [this.String];
+				this.events = [this.HasMany(this.models.Event)];
+			}
+			app.model(User);
+
 			function App() {
 				this.ignore = [this.Boolean];
 				this.events = [this.HasMany(this.models.Event), this.Private];
@@ -18,16 +27,21 @@ describe('models find with association property', function() {
 
 			function Event() {
 				this.test = [this.String];
+				this.createdAt = [this.DateTime, this.Default('CURRENT_TIMESTAMP')];
 				this.app = [this.BelongsTo(this.models.App)];
+				this.user = [this.BelongsTo(this.models.User)];
 			}
 			app.model(Event);
 		};
 
 		helper.createModels = function(app) {
-			return app.models.App.create({ignore: true})
-				.then(function(appModel) {
+			return Q.all([app.models.App.create({ignore: true}), app.models.User.create({name: 'Martijn'})])
+				.spread(function(appModel, user) {
+					userID = user.id;
+
 					return app.models.Event.create({
-						app: appModel
+						app: appModel,
+						user: user
 					});
 				});
 		};
@@ -42,6 +56,13 @@ describe('models find with association property', function() {
 
 	it('can find with ignore true', function() {
 		return helper.app.models.Event.find({test: null, 'app.ignore': true}, {limit:20})
+			.then(function(events) {
+				assert.equal(events.length, 1);
+			});
+	});
+
+	it('can find with ignore and orderBy', function() {
+		return helper.app.models.Event.find({test: null, 'app.ignore': true, user: userID}, {orderBy:{createdAt: 1}, limit: 600})
 			.then(function(events) {
 				assert.equal(events.length, 1);
 			});
