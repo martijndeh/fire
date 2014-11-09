dd673e09b6ffb042bfbfbb242a0273c5ca2820e7
martijndeh <martijn@ff00ff.nl>
2014-11-09 23:32:20 +0100

    updates triggers to use clock module

--- test/triggers.js
+++ test/triggers.js
@@ -6,82 +6,155 @@ var assert = require('assert');
 var Q = require('q');
 
 describe('triggers', function() {
-	/*
-	var myTriggerModelInstance = null;
+	var myTriggerModelInstance, myTriggerCalled;
 
 	beforeEach(helper.beforeEach());
 	afterEach(helper.afterEach());
 
-	before(function() {
-		helper.setup = function() {
-			myTriggerModelInstance = null;
+	describe('user model', function() {
+		before(function() {
+			helper.setup = function() {
+				myTriggerModelInstance = null;
+				myTriggerCalled = 0;
 
-			function User() {
-				this.name = [this.String, this.Authenticate];
-				this.events = [this.HasMany(this.models.Event)];
-			}
-			helper.app.model(User);
-
-			function Event() {
-				this.name = [this.String];
-				this.user = [this.BelongsTo(this.models.User)];
-			}
-			helper.app.model(Event);
-
-			function MyTrigger() {
-				//
-			}
-			helper.app.trigger(MyTrigger);
-
-			MyTrigger.prototype.where = function(modelInstance) {
-				return this.models.User.findOne({})
-			};
-
-			MyTrigger.prototype.run = function(modelInstance) {
-				if(this.when(modelInstance)) {
-					myTriggerModelInstance = modelInstance;
-
-					return this.activate(modelInstance.id, {
-						persistent: true,
-						resetAt: new Date()
-					});
+				function User() {
+					this.name = [this.String, this.Authenticate];
+					this.events = [this.HasMany(this.models.Event)];
 				}
+				helper.app.model(User);
+
+				function Event() {
+					this.name = [this.String];
+					this.user = [this.BelongsTo(this.models.User)];
+				}
+				helper.app.model(Event);
+
+				function MyTrigger() {
+					this.timingPattern = '* * * * * *';
+				}
+				helper.app.trigger(MyTrigger);
+
+				MyTrigger.prototype.select = function() {
+					return {name: 'Martijn'};
+				};
+
+				MyTrigger.prototype.run = function(user) {
+					myTriggerCalled++;
+					myTriggerModelInstance = user;
+				};
 			};
-		};
-		helper.createModels = null;
-	});
 
-	it('can invoke trigger', function() {
-		assert.equal(myTriggerModelInstance, null);
-
-		return helper.app.models.User.create({
-			name: 'Martijn',
-			password: 'test'
-		})
-		.then(function() {
-			return Q.delay(100);
-		})
-		.then(function() {
-			assert.notEqual(myTriggerModelInstance, null);
-			assert.equal(myTriggerModelInstance.name, 'Martijn');
+			helper.createModels = function() {
+				return helper.app.models.User.create([{
+					name: 'Martijn',
+					password: 'test'
+				}, {
+					name: 'Not Martijn',
+					password: 'test'
+				}]);
+			};
 		});
-	});
 
-	it('will not invoke trigger', function() {
-		assert.equal(myTriggerModelInstance, null);
+		it('sets interval correctly', function() {
+			assert.equal(helper.app.triggers._triggersMap.MyTrigger.timingPattern, '* * * * * *');
+		});
 
-		return helper.app.models.User.create({
-			name: 'Not Martijn',
-			password: 'test'
-		})
-		.then(function() {
-			return Q.delay(100);
-		})
-		.then(function() {
+		it('can invoke trigger', function() {
 			assert.equal(myTriggerModelInstance, null);
+			assert.notEqual(helper.app.triggers._triggersMap.MyTrigger, null);
+
+			helper.app.triggers.startConsuming();
+
+			return helper.app.triggers.startTrigger(helper.app.triggers._triggersMap.MyTrigger)
+				.then(function() {
+					return Q.delay(100);
+				})
+				.then(function() {
+					assert.notEqual(myTriggerModelInstance, null);
+					assert.equal(myTriggerModelInstance.name, 'Martijn');
+				});
+		});
+
+		it('will not invoke trigger twice', function() {
+			assert.equal(myTriggerModelInstance, null);
+			assert.equal(myTriggerCalled, 0);
+
+			helper.app.triggers.startConsuming();
+
+			return helper.app.triggers.startTrigger(helper.app.triggers._triggersMap.MyTrigger)
+				.then(function() {
+					return Q.delay(100);
+				})
+				.then(function() {
+					assert.notEqual(myTriggerModelInstance, null);
+
+					assert.equal(myTriggerCalled, 1);
+					assert.equal(myTriggerModelInstance.name, 'Martijn');
+				})
+				.then(function() {
+					return helper.app.triggers.startTrigger(helper.app.triggers._triggersMap.MyTrigger);
+				})
+				.then(function() {
+					return Q.delay(100);
+				})
+				.then(function() {
+					assert.equal(myTriggerCalled, 1);
+					assert.equal(myTriggerModelInstance.name, 'Martijn');
+				});
 		});
 	});
 
-	// will only fire once, option to reset trigger, timeout
-	*/
+	describe('test model', function() {
+		before(function() {
+			helper.setup = function() {
+				myTriggerModelInstance = null;
+				myTriggerCalled = 0;
+
+				function Test() {
+					this.name = [this.String, this.Authenticate];
+				}
+				helper.app.model(Test);
+
+				function MyTrigger() {
+					//
+				}
+				helper.app.trigger(MyTrigger);
+
+				MyTrigger.prototype.select = function() {
+					return {name: 'Martijn'};
+				};
+
+				MyTrigger.prototype.run = function(test) {
+					myTriggerCalled++;
+					myTriggerModelInstance = test;
+				};
+			};
+
+			helper.createModels = function() {
+				return helper.app.models.Test.create([{
+					name: 'Martijn',
+					password: 'test'
+				}, {
+					name: 'Not Martijn',
+					password: 'test'
+				}]);
+			};
+		});
+
+		it('can invoke trigger', function() {
+			assert.equal(myTriggerModelInstance, null);
+			assert.notEqual(helper.app.triggers._triggersMap.MyTrigger, null);
+
+			helper.app.triggers.startConsuming();
+
+			return helper.app.triggers.startTrigger(helper.app.triggers._triggersMap.MyTrigger)
+				.then(function() {
+					return Q.delay(100);
+				})
+				.then(function() {
+					assert.notEqual(myTriggerModelInstance, null);
+					assert.equal(myTriggerModelInstance.name, 'Martijn');
+				});
+		});
+	});
 });
