140c5d9b37d66174573c6a7853832f13fc10c494
martijndeh <martijn@ff00ff.nl>
2015-01-02 23:09:09 +0100

    extends dependency injection

--- test/injector.js
+++ test/injector.js
@@ -3,7 +3,6 @@
 
 var helper = require('./support/helper');
 var assert = require('assert');
-var helper = require('./support/helper');
 
 describe('injector', function() {
 	beforeEach(helper.beforeEach());
@@ -39,6 +38,30 @@ describe('injector', function() {
 
 		function TestConstructor(self) {
 			assert.equal(self, this);
+			called++;
+		}
+
+		helper.app.injector.execute(TestConstructor, {});
+		assert.equal(called, 1);
+	});
+
+	it('can inject knex', function() {
+		var called = 0;
+
+		function TestConstructor(knex) {
+			assert.notEqual(knex, null);
+			called++;
+		}
+
+		helper.app.injector.execute(TestConstructor, {});
+		assert.equal(called, 1);
+	});
+
+	it('can inject app', function() {
+		var called = 0;
+
+		function TestConstructor(app) {
+			assert.equal(app, helper.app);
 			called++;
 		}
 
