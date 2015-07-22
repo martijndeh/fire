5b707b15365f3ff631b1cd3191c587e1e76a8a88
martijndeh <martijn@ff00ff.nl>
2015-07-22 11:21:27 +0200

    updates multiple apps example project

---
+++ examples/multiple-apps/.fire/.build/api/app1/user-in-app1.js
@@ -0,0 +1,370 @@
+'use strict';
+
+var Q = require('q');
+
+var fire = require('fire');
+var app = fire.app('nodeonfire.org');
+
+var http = require('http');
+
+function merge(dest, source) {
+	Object.keys(source).forEach(function(key) {
+		dest[key] = source[key];
+	});
+	return dest;
+}
+
+function unauthenticatedError(authenticator) {
+	var error = new Error();
+
+	if(authenticator) {
+		error.status = 403;
+	}
+	else {
+		error.status = 401;
+	}
+
+	error.message = http.STATUS_CODES[error.status];
+	return error;
+}
+
+function badRequestError() {
+	var error = new Error();
+	error.status = 400;
+	error.message = http.STATUS_CODES[error.status];
+	return error;
+}
+
+function _canUpdateProperties(propertyNames, model) {
+	for(var i = 0, il = propertyNames.length; i < il; i++) {
+		var propertyName = propertyNames[i];
+		var property = model.getProperty(propertyName);
+
+		// TODO: Implement function-based checks.
+		if(property && (typeof property.options.canUpdate != 'undefined' && property.options.canUpdate !== true || typeof property.options.canSet != 'undefined' && property.options.canSet !== true)) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+function _canSetProperties(propertyNames, model) {
+	for(var i = 0, il = propertyNames.length; i < il; i++) {
+		var propertyName = propertyNames[i];
+		var property = model.getProperty(propertyName);
+
+		// TODO: Implement function-based checks.
+		if(property && typeof property.options.canSet != 'undefined' && property.options.canSet !== true) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+function findAuthenticator(authenticatorModel, request) {
+	if(!authenticatorModel) {
+		return Q.when(null);
+	}
+
+	var credentials = null;
+	if(request.headers.authorization && request.headers.authorization.length > 6) {
+		credentials = (new Buffer(request.headers.authorization.substring(6), 'base64')).toString('utf8').split(':');
+
+		if(!credentials.length) {
+			credentials = null;
+		}
+		else if(credentials.length == 1) {
+			credentials.push('');
+		}
+	}
+
+	if(credentials) {
+		var findMap = {};
+		findMap[authenticatorModel.options.authenticatingProperty.name] = credentials[0];
+		findMap.accessToken = credentials[1];
+		return authenticatorModel.findOne(findMap);
+	}
+
+	if(!request.session.at) {
+		return Q.when(null);
+	}
+
+	return authenticatorModel.findOne({accessToken: request.session.at});
+}
+
+
+
+
+
+app.post('/api/user-in-app1s', function(app, response, request, UserInApp1Model) {
+	return findAuthenticator(null, request)
+		.then(function(authenticator) {
+			var accessControl = UserInApp1Model.getAccessControl();
+			return Q.when(accessControl.canCreate({authenticator: authenticator, request: request, response: response}))
+				.then(function(canCreate) {
+					if(canCreate) {
+						var checkCreateMap = function(createMap) {
+							if(typeof canCreate == 'object') {
+								createMap = merge(createMap, canCreate);
+							}
+
+							if(UserInApp1Model.options.automaticPropertyName) {
+								createMap[UserInApp1Model.options.automaticPropertyName] = authenticator;
+							}
+
+
+
+							if(_canSetProperties(Object.keys(createMap), UserInApp1Model)) {
+								return createMap;
+							}
+							else {
+								throw badRequestError();
+							}
+						};
+
+						if(Array.isArray(request.body)) {
+							
+
+							var createMaps = request.body.map(function(createMap) {
+								return checkCreateMap(createMap);
+							});
+
+							return UserInApp1Model.create(createMaps, {authenticator: authenticator, request: request, response: response});
+							
+						}
+						else {
+							return UserInApp1Model.create(checkCreateMap(request.body || {}), {authenticator: authenticator, request: request, response: response});
+						}
+					}
+					else {
+						throw unauthenticatedError(authenticator);
+					}
+				});
+		});
+});
+
+app.get('/api/user-in-app1s', function(request, response, app,  UserInApp1Model) {
+	return findAuthenticator(null, request)
+		.then(function(authenticator) {
+			var whereMap = request.query || {};
+			var optionsMap = {};
+
+			if(whereMap.$options) {
+				optionsMap = whereMap.$options;
+				delete whereMap.$options;
+			}
+			optionsMap.isShallow = true;
+
+			if(UserInApp1Model.options.automaticPropertyName) {
+				whereMap[UserInApp1Model.options.automaticPropertyName] = authenticator;
+			}
+
+			var accessControl = UserInApp1Model.getAccessControl();
+			return Q.when(accessControl.canRead({authenticator: authenticator, request: request, response: response, whereMap: whereMap}))
+				.then(function(canRead) {
+					if(canRead) {
+						if(typeof canRead == 'object') {
+							whereMap = merge(whereMap, canRead);
+						}
+
+						return UserInApp1Model.find(whereMap, optionsMap);
+					}
+					else {
+						throw unauthenticatedError(authenticator);
+					}
+				});
+		});
+});
+
+app.get('/api/user-in-app1s/:id', function(request, response, app,  UserInApp1Model) {
+	return findAuthenticator(null, request)
+		.then(function(authenticator) {
+			var whereMap = request.query || {};
+			whereMap.id = request.params.id;
+
+			if(UserInApp1Model.options.automaticPropertyName) {
+				whereMap[UserInApp1Model.options.automaticPropertyName] = authenticator;
+			}
+
+			var optionsMap = {};
+
+			if(whereMap.$options) {
+				optionsMap = whereMap.$options;
+				delete whereMap.$options;
+			}
+
+			optionsMap.isShallow = true;
+
+			var accessControl = UserInApp1Model.getAccessControl();
+			return Q.when(accessControl.canRead({authenticator: authenticator, request: request, response: response, whereMap: whereMap}))
+				.then(function(canRead) {
+					if(canRead) {
+						if(typeof canRead == 'object') {
+							whereMap = merge(whereMap, canRead);
+						}
+
+						return UserInApp1Model.getOne(whereMap, optionsMap);
+					}
+					else {
+						throw unauthenticatedError(authenticator);
+					}
+				});
+		});
+});
+
+app.put('/api/user-in-app1s/:id', function(request, response, app,  UserInApp1Model) {
+	var accessControl = UserInApp1Model.getAccessControl();
+	return findAuthenticator(null, request)
+		.then(function(authenticator) {
+			var whereMap = request.query || {};
+
+			if(UserInApp1Model.options.automaticPropertyName) {
+				whereMap[UserInApp1Model.options.automaticPropertyName] = authenticator;
+			}
+
+			whereMap.id = request.params.id;
+
+			return Q.when(accessControl.canUpdate({authenticator: authenticator, request: request, response: response, whereMap: whereMap}))
+				.then(function(canUpdate) {
+					if(canUpdate) {
+						if(typeof canUpdate == 'object') {
+							whereMap = merge(whereMap, canUpdate);
+						}
+
+						return [_canUpdateProperties(Object.keys(request.body), UserInApp1Model), whereMap, authenticator];
+					}
+					else {
+						throw unauthenticatedError(authenticator);
+					}
+				});
+		})
+		.all()
+		.spread(function(canUpdateProperties, whereMap, authenticator) {
+			if(canUpdateProperties) {
+				return Q.all([UserInApp1Model.updateOne(whereMap, request.body), authenticator]);
+			}
+			else {
+				throw badRequestError();
+			}
+		})
+		.spread(function(modelInstance, authenticator) {
+			if(modelInstance) {
+				return modelInstance;
+			}
+			else {
+				throw unauthenticatedError(authenticator);
+			}
+		})
+		.catch(function(error) {
+			throw error;
+		});
+});
+
+app.put('/api/user-in-app1s', function(request, response, app,  UserInApp1Model) {
+	return findAuthenticator(null, request)
+		.then(function(authenticator) {
+			var whereMap = request.query || {};
+
+			if(UserInApp1Model.options.automaticPropertyName) {
+				whereMap[UserInApp1Model.options.automaticPropertyName] = authenticator;
+			}
+
+			var accessControl = UserInApp1Model.getAccessControl();
+			return Q.when(accessControl.canUpdate({authenticator: authenticator, request: request, response: response, whereMap: whereMap}))
+				.then(function(canUpdate) {
+					if(canUpdate) {
+						return Q.when(_canUpdateProperties(Object.keys(request.body || {}), UserInApp1Model))
+							.then(function(canUpdateProperties) {
+								if(canUpdateProperties) {
+									if(typeof canUpdate == 'object') {
+										whereMap = merge(whereMap, canUpdate);
+									}
+
+									return UserInApp1Model.update(whereMap, request.body || {});
+								}
+								else {
+									throw badRequestError();
+								}
+							});
+					}
+					else {
+						throw unauthenticatedError(authenticator);
+					}
+				});
+		});
+});
+
+app.delete('/api/user-in-app1s', function(request, response, app,  UserInApp1Model) {
+	return findAuthenticator(null, request)
+		.then(function(authenticator) {
+			var whereMap = request.query || {};
+			if(UserInApp1Model.options.automaticPropertyName) {
+				whereMap[UserInApp1Model.options.automaticPropertyName] = authenticator;
+			}
+
+			var optionsMap = null;
+			if(whereMap.$options) {
+                optionsMap = whereMap.$options;
+                delete whereMap.$options;
+            }
+
+			var accessControl = UserInApp1Model.getAccessControl();
+			return Q.when(accessControl.canDelete({authenticator: authenticator, request: request, response: response, whereMap: whereMap}))
+				.then(function(canDelete) {
+					if(canDelete) {
+						if(typeof canDelete == 'object') {
+							whereMap = merge(whereMap, canDelete);
+						}
+
+						return UserInApp1Model.remove(whereMap, optionsMap);
+					}
+					else {
+						throw unauthenticatedError(authenticator);
+					}
+				});
+		});
+});
+
+app.delete('/api/user-in-app1s/:id', function(request, response, app,  UserInApp1Model) {
+	return findAuthenticator(null, request)
+		.then(function(authenticator) {
+			var whereMap = request.query || {};
+			whereMap.id = request.params.id;
+			if(UserInApp1Model.options.automaticPropertyName) {
+				whereMap[UserInApp1Model.options.automaticPropertyName] = authenticator;
+			}
+
+			var optionsMap = null;
+			if(whereMap.$options) {
+                optionsMap = whereMap.$options;
+                delete whereMap.$options;
+            }
+
+			var accessControl = UserInApp1Model.getAccessControl();
+			return Q.when(accessControl.canDelete({authenticator: authenticator, request: request, response: response, whereMap: whereMap}))
+			.then(function(canDelete) {
+				if(canDelete) {
+					if(typeof canDelete == 'object') {
+						whereMap = merge(whereMap, canDelete);
+					}
+
+					return UserInApp1Model.removeOne(whereMap, optionsMap);
+				}
+				else {
+					throw unauthenticatedError(authenticator);
+				}
+			});
+		});
+});
+
+
+
+
+
+
+
+
+
+
--- examples/multiple-apps/index.js
+++ examples/multiple-apps/index.js
@@ -1,6 +1,21 @@
 var fire = require('fire');
 
+/**
+ * We create the `app1` app and set it as the master app.
+ *
+ * The master app keeps tracks of all migrations of your shared models.
+ */
 var app1 = fire.app('nodeonfire.org', 'app1', {isMaster: true});
+
+/**
+ * We create another app: `app2`.
+ *
+ * By setting NODE_APP=app2, building and running the project we can start `app2`.
+ *
+ * Tip! You can use the below command to quickly set the NODE_APP environment variable:
+ *
+ * 	`fire config:set NODE_APP=app2`
+ */
 var app2 = fire.app('nodeonfire.org', 'app2', {});
 
 app1.controller('/', function TestController() {
