d41a6b8f5690c241c7973cd310a33957e7503515
martijndeh <martijn@ff00ff.nl>
2015-08-08 17:14:04 +0200

    updates website

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,6 @@
 exports = module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-build-html');
 
 	grunt.initConfig({
 		less: {
@@ -10,6 +11,49 @@ exports = module.exports = function(grunt) {
 				files: {
 					'styles/default.min.css': 'styles/default.less'
 				}
+			}
+		},
+
+		buildHtml: {
+			index: {
+				options: {
+					templates: './templates/*.html'
+				},
+				expand: false,
+				cwd: './',
+				src: 'site/index.html',
+				dest: 'index.html',
+				ext: '.html'
+			},
+			examples: {
+				options: {
+					templates: './templates/*.html'
+				},
+				expand: false,
+				cwd: './',
+				src: 'site/examples.html',
+				dest: 'examples.html',
+				ext: '.html'
+			},
+			'getting-started': {
+				options: {
+					templates: './templates/*.html'
+				},
+				expand: false,
+				cwd: './',
+				src: 'site/getting-started.html',
+				dest: 'getting-started.html',
+				ext: '.html'
+			},
+			'docs-layout': {
+				options: {
+					templates: './templates/*.html'
+				},
+				expand: false,
+				cwd: './',
+				src: 'site/docs-layout.html',
+				dest: '_template/tmpl/layout.tmpl',
+				ext: '.html'
 			}
 		}
 	});
