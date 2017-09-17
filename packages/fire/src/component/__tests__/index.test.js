c143f547647602c01794d37bc70e75998f325539
Martijn de Haan <martijn@ff00ff.nl>
2017-09-17 20:16:27 +0200

    test(fire): remove error code

--- packages/fire/src/component/__tests__/index.test.js
+++ packages/fire/src/component/__tests__/index.test.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/no-multi-comp */
 import React from 'react';
-import component, { getPathForErrorCode, isComponent } from '..';
+import { isComponent } from '..';
 
 describe(`component`, () => {
     it(`React.Component should be component`, () => {
@@ -9,29 +9,9 @@ describe(`component`, () => {
         expect(isComponent(MyComponent)).toBe(true);
     });
 
-    it(`React.Component should not be component`, () => {
+    it(`Class should not be component`, () => {
         class SomeClass {}
 
         expect(isComponent(SomeClass)).toBe(false);
     });
-
-    it(`should get path for error code 404`, () => {
-        class MyComponent extends React.Component {}
-
-        component(`/404`, { error: 404 })(MyComponent);
-
-        const path = getPathForErrorCode(404);
-
-        expect(path).toBe(`/404`);
-    });
-
-    it(`should get path for from multiple error codes`, () => {
-        class MyComponent extends React.Component {}
-
-        component(`/error-page`, { error: [400, 401, 402, 403] })(MyComponent);
-
-        const path = getPathForErrorCode(403);
-
-        expect(path).toBe(`/error-page`);
-    });
-})
+});
