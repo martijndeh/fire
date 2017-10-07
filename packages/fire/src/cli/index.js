f18107d4e5a85504317558f6f9cb8a1c2a75e1fa
Martijn de Haan <martijn@ff00ff.nl>
2017-10-07 23:40:18 +0200

    feat(fire): disable postgres ssl in development

--- packages/fire/src/cli/index.js
+++ packages/fire/src/cli/index.js
@@ -19,6 +19,10 @@ function main() {
 
     const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV === `development`;
 
+    if (isDevelopment && !process.env.LEGO_DISABLE_SSL) {
+        process.env.LEGO_DISABLE_SSL = `true`;
+    }
+
     try {
         process.env.FIRE_STAGE = topic;
 
